7d72c24b760a55bb60962ef825bd9f54fe43e44d
David Zoufalý <davidzoufaly@gmail.com>
2018-11-28 22:01:21 +0100

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@
 // ? what is goin on here
 // default comments
 
-
 import React, { Component } from 'react';
 import './App.css';
 
@@ -82,7 +81,8 @@ class Filter extends Component {
     return (
       <div style={defaultStyle}>
         <img/>
-        <input type="text"/>
+        <input type="text" onKeyUp={event => 
+          this.props.onTextChange(event.target.value)}/>
       </div>
     );
   }
@@ -108,7 +108,10 @@ class Playlist extends Component {
 class App extends Component {
   constructor() {
     super();
-    this.state = {serverData: {}}  
+    this.state = {
+      serverData: {},
+      filterString: ''
+    }  
   }
   componentDidMount() {
     setTimeout(() => {
@@ -134,10 +137,17 @@ class App extends Component {
         </h1>
         <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
         <HoursCounter playlists={this.state.serverData.user.playlists}/>
-        <Filter/>
-        {this.state.serverData.user.playlists.map(playlist => 
-          <Playlist playlist={playlist}/>
-        )}
+        <Filter onTextChange={text => this.setState({filterString: text})}/>
+        {this.state.serverData.user.playlists
+            .filter(
+              playlist => 
+                playlist.name.toLowerCase().includes(
+                  this.state.filterString.toLocaleLowerCase())
+            )
+            .map(
+              playlist => 
+                <Playlist playlist={playlist}/>
+              )}
         </div> : <h1 style={defaultStyle}>Loading...</h1> }
       </div>
     );
